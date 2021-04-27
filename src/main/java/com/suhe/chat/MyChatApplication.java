58ea193b2015748ef605fe7cf04ee7ecf0121603
mateusz <mateusz.suszczynski@gmail.com>
2021-04-27 21:10:06 +0200

    added reactor
    Added reactor-core dependency to make chat work asynchronously

--- src/main/java/com/suhe/chat/MyChatApplication.java
+++ src/main/java/com/suhe/chat/MyChatApplication.java
@@ -1,7 +1,11 @@
 package com.suhe.chat;
 
+import com.suhe.chat.backend.ChatMessage;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.UnicastProcessor;
 
 @SpringBootApplication
 public class MyChatApplication {
@@ -10,4 +14,12 @@ public class MyChatApplication {
         SpringApplication.run(MyChatApplication.class, args);
     }
 
+    @Bean
+    UnicastProcessor<ChatMessage> publisher(){
+        return UnicastProcessor.create();
+    }
+    @Bean
+    Flux<ChatMessage> messages(UnicastProcessor<ChatMessage> publisher) {
+        return publisher.replay(30).autoConnect();
+    }
 }
--- src/main/java/com/suhe/chat/ui/ChatView.java
+++ src/main/java/com/suhe/chat/ui/ChatView.java
@@ -11,28 +11,34 @@ import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.html.Paragraph;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Push;
 import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.UnicastProcessor;
 
 
 /**
- *  Ways to exchange messages inside component (UI)
+ *  Ways to exchange messages inside component (UI) within one session
  *  - set chat component into field, so passing pojo would be available.
  *  - binder
  *  - custom events
+ *
  *
  */
 
 @Route("")
 @CssImport("./styles/styles.css")
+@Push
 public class ChatView extends VerticalLayout {
-    private String username = "ADMIN";
+    private String username;
+    private final UnicastProcessor<ChatMessage> publisher;
+    private final Flux<ChatMessage> messages;
 
 
-    ChatMessage chatMessage;
-    MessageList messageList;        // for test's purpose
-
-    public ChatView() {
+    public ChatView(UnicastProcessor<ChatMessage> publisher, Flux<ChatMessage> messages) {
+        this.publisher = publisher;
+        this.messages = messages;
         setSizeFull();
         setDefaultHorizontalComponentAlignment(Alignment.CENTER);
         addClassName("main-view");
@@ -42,8 +48,7 @@ public class ChatView extends VerticalLayout {
         
         add(header);
 
-//        singInLayout();
-        chatLayout();
+        singInLayout();
     }
 
     private void singInLayout() {
@@ -53,55 +58,48 @@ public class ChatView extends VerticalLayout {
         layout.add(usernameField, startButton);
         add(layout);
 
+        startButton.addClickShortcut(Key.ENTER);
         startButton.addClickListener(click -> {
             this.username = usernameField.getValue();
             remove(layout);
             chatLayout();
         });
-        startButton.addClickShortcut(Key.ENTER);
+
     }
 
     private void chatLayout() {
-
-        // need to implement a subscriber so messageList could be updated whenever new message occur.
-        // messageList.add(new Paragraph(message.getFrom() + " " + message.getMessage()));
-        messageList = new MessageList();
+        MessageList messageList = new MessageList();
         add(messageList, inputLayout());
 
-        expand(messageList);
+        messages.subscribe(message -> {
+            getUI().ifPresent(ui ->
+                    ui.access(() ->
+                            messageList.add(new Paragraph(
+                    message.getFrom() + " " + message.getMessage()))
+            ));
+        });
 
+        expand(messageList);
     }
 
     private Component inputLayout() {
         HorizontalLayout layout = new HorizontalLayout();
-        layout.setWidthFull();
+
         TextField messageField = new TextField();
-
-
         Button sendButton = new Button("Send");
         sendButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
-        layout.add(messageField, sendButton);
-
-        layout.expand(messageField);
-
+        sendButton.addClickShortcut(Key.ENTER);
         sendButton.addClickListener(click -> {
-            // I need to publish a message here, or send it to the publisher
-            ChatMessage message = new ChatMessage(username, messageField.getValue());
-            // this one is for tests if UI works properly
-            messageList.add(new Paragraph(message.getFrom() + " " + message.getMessage()));
+            publisher.onNext(new ChatMessage(username, messageField.getValue()));
             messageField.clear();
             messageField.focus();
         });
-        sendButton.addClickShortcut(Key.ENTER);
         messageField.focus();
+
+        layout.add(messageField, sendButton);
+        layout.setWidthFull();
+        layout.expand(messageField);
         return layout;
-    }
-
-    public static class ChatMessageEvent extends ComponentEvent<ChatView> {
-
-        public ChatMessageEvent(ChatView source, boolean fromClient) {
-            super(source, false);
-        }
     }
 
 }
